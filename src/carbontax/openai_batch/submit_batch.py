5a748e17010727864013199c04f654061902b473
GZ <zheng.gong94@gmail.com>
2026-07-23 01:14:32 +0200

    nit

--- src/carbontax/openai_batch/submit_batch.py
+++ src/carbontax/openai_batch/submit_batch.py
@@ -4,10 +4,12 @@ from carbontax.config import load_run_config
 from carbontax.openai_batch.batch_job import OpenAIBatchJob
 from carbontax.utils.logger import setup_logging
 
+CONFIG_PATH = "config/run_test_trucost.yaml"
+
 
 def main() -> None:
     setup_logging()
-    OpenAIBatchJob(load_run_config()["run_name"]).submit()
+    OpenAIBatchJob(load_run_config(CONFIG_PATH)["run_name"]).submit()
 
 
 if __name__ == "__main__":
